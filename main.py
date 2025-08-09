85a148b9f03b0ab500c8e9f94d87e1401f3c1c5a
Hendra Dwi Saputra <babeguru@gmail.com>
2025-08-09 11:58:05 +0700

    Flet sample code for web application

---
+++ main.py
@@ -0,0 +1,35 @@
+from dataclasses import dataclass
+
+import flet as ft
+
+
+@dataclass
+class AppState:
+    count: int
+
+    def increment(self):
+        self.count += 1
+
+
+def main(page: ft.Page):
+    state = AppState(count=0)
+
+    page.floating_action_button = ft.FloatingActionButton(
+        icon=ft.Icons.ADD, on_click=state.increment
+    )
+    page.add(
+        ft.ControlBuilder(
+            state,
+            lambda state: ft.SafeArea(
+                ft.Container(
+                    ft.Text(value=f"{state.count}", size=50),
+                    alignment=ft.Alignment.CENTER,
+                ),
+                expand=True,
+            ),
+            expand=True,
+        )
+    )
+
+
+ft.run(main, view=ft.AppView.WEB_BROWSER, host="127.0.0.1", port=8550)
